e9785847674a0f057187360cd89515f8d3406983
Asher950519 <92915782+Asher950519@users.noreply.github.com>
2023-04-28 21:35:07 +0800

    Create ex2-4.cpp

---
+++ ex2-4.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){
+    // int a,b;
+    // printf("A=> ");
+    // scanf("%d", &a);
+    // printf("B=> ");
+    // scanf("%d", &b);
+    // printf("A+B=%d\n",a+b);
+    // printf("A-B=%d\n",a-b);
+    // printf("A*B=%d\n",a*b);
+    // printf("A/B=%d\n",a/b);
+    
+    float c,d;
+    printf("C=> ");
+    scanf("%f", &c);
+    printf("D=> ");
+    scanf("%f", &d);
+    printf("C+D=%f\n",c+d);
+    printf("C-D=%f\n",c-d);
+    printf("C*D=%f\n",c*d);
+    printf("C/D=%f\n",c/d);
+    return 0;
+}
