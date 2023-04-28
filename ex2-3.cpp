06c7dd80d7b505e3393d09c379975e0bb45e6bcc
Asher950519 <92915782+Asher950519@users.noreply.github.com>
2023-04-28 21:33:59 +0800

    Create ex2-3.cpp

---
+++ ex2-3.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int height_m=1, height_cm=80;
+    float f_height=1.80;
+    char a='b';
+    printf("hello Asher ");
+    printf("my height is %d m %d cm \n",height_m,height_cm);
+    printf("my height is %f m \n",f_height);
+    printf("%c \n",a);
+    return 0;
+}
