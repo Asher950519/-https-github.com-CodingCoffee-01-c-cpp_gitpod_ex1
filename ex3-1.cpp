43ef445af4bda55703141d65c9bf1ecd92e35d18
Asher950519 <92915782+Asher950519@users.noreply.github.com>
2023-05-05 21:04:51 +0800

    Create ex3-1.cpp

---
+++ ex3-1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i,j,a,b;
+    scanf("%d %d %d %d",&i,&j,&a,&b);
+    printf("%d \n",i++);
+    printf("%d \n",j++);
+    printf("%d \n",i);
+    printf("%d \n",j);    
+    printf("%d \n",++a);
+    printf("%d \n",++b);
+    return 0;  
+}
