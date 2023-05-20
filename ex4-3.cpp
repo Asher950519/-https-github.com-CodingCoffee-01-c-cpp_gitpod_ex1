01c177d54dc5784d96cc661677f0d11f0e38e44d
Asher950519 <92915782+Asher950519@users.noreply.github.com>
2023-05-20 10:42:33 +0800

    Create ex4-3.cpp

---
+++ ex4-3.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int r; //半徑
+    scanf("%d",&r);
+    if (r>=0){
+        printf("Area=%f \n",r*r*3.14);
+        printf("Length=%f \n",r*2*3.14);
+    }
+    else
+        printf("incorrect \n");
+  
+  
+        
+    int y;  //閏年判斷
+    scanf("%d",&y);
+    if (y%4==0 && y%100!=0)
+        printf("Yes! It's a leap year.\n");
+    else if (y%400==0) 
+        printf("Yes! It's a leap year.\n");
+    else
+        printf("No. It's a normal year.\n");
+
+
+        
+    int y1,m; // 年月天數判斷(bug)
+    scanf("%d %d",&y1,&m);
+    if (y1%4==0 && y1%100!=0)
+        if(m==2)
+            printf("29");
+    else if (y1%400==0)
+        if(m==2)
+            printf("29");
+    else 
+        if(m==2)
+            printf("28");
+        else if (m==1 || m==3 || m==5 || m==7 || m==8 || m==10 || m==12)
+            printf("31");
+        else if (m==4 || m==6 || m==9 || m==11)
+            printf("30");
+        else
+            printf("Error");
+        
+    
+}
