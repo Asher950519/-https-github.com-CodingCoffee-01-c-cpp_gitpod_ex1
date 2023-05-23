c1bb4d51864896f68b3c3febfec0faaada3846ac
Asher950519 <92915782+Asher950519@users.noreply.github.com>
2023-05-23 20:01:15 +0800

    Create test 5-4.cpp

---
+++ test 5-4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int m,f;
+    scanf("%d %d",&m,&f);
+    if(m>f){
+        printf("I am senior: %d",m);
+    }else{
+        printf("My friend is senior: %d",f);
+    }
+
+    return 0;
+}
