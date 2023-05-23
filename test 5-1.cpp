1f2b75e5446bcac0b83042fa87ce3accd232d8c7
Asher950519 <92915782+Asher950519@users.noreply.github.com>
2023-05-23 19:59:24 +0800

    Create test 5-1.cpp

---
+++ test 5-1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float a,b,c,d,e,f,g,h,i,j,sum;
+    
+    scanf("%f %f %f %f %f %f %f %f %f %f",&a,&b,&c,&d,&e,&f,&g,&h,&i,&j);
+    sum=(a+b+c+d+e+f+g+h+i+j)/10;
+    printf("%.1f",sum);
+    
+
+    return 0;
+}
