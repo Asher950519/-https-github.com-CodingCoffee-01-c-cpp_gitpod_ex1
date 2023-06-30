6650357bddf22d7c3d77bd56fa4996dc778aeae2
Asher950519 <92915782+Asher950519@users.noreply.github.com>
2023-06-30 21:56:28 +0800

    Create ex10-1.cpp

---
+++ ex10-1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a[6],i;
+    for(i=0;i<6;i++){
+        a[i]=i;
+    }
+    
+    printf("a[%d]=%d \n",4,a[4]);
+
+    return 0;
+}
